9071225d4fe407217708c2ced4d7776c2bae03f8
Dipesh_Maywade <dipesh@ideaclan.biz>
2021-08-23 13:58:01 +0530

    use evnt.target.value as a changHandeler

--- src/App.js
+++ src/App.js
@@ -24,17 +24,29 @@ function App() {
     })
   }
 
+  const nameChangeHandler = (event) => {
+    //console.log("was clicked");
+    setPersonData({
+      persons: [
+        {name: "Max", age : 28},
+        {name: event.target.value, age: 26},
+        {name: "Deep_update",age : 22}
+      ]
+    })
+  }
+
   return (
     <div className="App">
       <h1>react app</h1>
-      <button onClick={() => switchHandler()}>switch Name</button>
+      <button onClick={() => switchHandler("Max_Using_callback")}>switch Name</button>
       <Person  
         name= {personData.persons[0].name} 
         age={personData.persons[0].age}
         click = {switchHandler.bind(this, "Max_Using_bind")}/>
       <Person 
         name={personData.persons[1].name} 
-        age={personData.persons[1].age}>This is child</Person>
+        age={personData.persons[1].age}
+        changed = {nameChangeHandler}>This is child</Person>
       <Person 
         name={personData.persons[2].name} 
         age={personData.persons[2].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,6 +6,7 @@ const Person = (props) => {
     <div className="Person">
       <p onClick={props.click}>I'm a person my name is {props.name} and age is {props.age}</p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed}></input>
     </div>
   );
 }
