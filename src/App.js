472d0b61e430efe6ed6d23787fb5b0719ac21bc5
Dipesh_Maywade <dipesh@ideaclan.biz>
2021-09-08 13:41:51 +0530

    dynamic listing daynamic styling radiaum

--- src/App.js
+++ src/App.js
@@ -1,67 +1,111 @@
 import './App.css';
 import Person from './Person/Person';
 import React, { useState } from 'react';
+import Radium from 'radium';
 
 
 function App() {
 
   const [personData, setPersonData] = useState({
     persons: [
-      {name: "Max", age : 28},
-      {name: "Manu", age : 26},
-      {name: "Deep", age : 22}
+      {id: "fdsfsd", name: "Max", age : 28},
+      {id: "sffsd",name: "Manu", age : 26},
+      {id: "2sfsd", name: "name", age : 22}
     ],
     showPersons: false
   })
 
-  const switchHandler = (arg) => {
-    console.log("was clicked");
-    setPersonData({
-      persons: [
-        {name: arg, age : 28},
-        {name: "Manu_update", age : 26},
-        {name: "Deep_update",age : 22}
-      ],
+  // const switchHandler = (arg) => {
+  //   setPersonData({...personData,
+  //     persons: [
+  //       {name: arg, age : 28},
+  //       {name: "Manu_update", age : 26},
+  //       {name: "Deep_update",age : 22}
+  //     ]
+  //   })
+  // }
 
-    })
+  const nameChangeHandler = (event, id) => {
+    const personIndex = personData.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {...personData.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...personData.persons]
+
+    persons[personIndex] = person;
+
+    setPersonData({ ...personData, persons:persons })
   }
 
-  const nameChangeHandler = (event) => {
-    //console.log("was clicked");
-    setPersonData({
-      persons: [
-        {name: "Max", age : 28},
-        {name: event.target.value, age: 26},
-        {name: "Deep_update",age : 22}
-      ]
-    })
+  const deletePerson = (index) => {
+    const persons = [...personData.persons];
+    persons.splice(index, 1);
+    setPersonData({...personData, persons: persons});
   }
+
 
   const togglePersonHandler = () => {
     const currentValue = personData.showPersons;
     setPersonData({...personData, showPersons: !currentValue})
   }
+
+  const style = {
+    backgroundColor:'green',
+    font: 'inherit',
+    border: '5px solid black',
+    padding: '5px',
+    cursor: 'pointer',
+    ':hover':{
+      backgroundColor: 'lightgreen',
+      color: 'balck'
+    }
+  }
+
+  let persons = null; 
+
+  if (personData.showPersons ) {
+    persons = (
+      <div>
+        {personData.persons.map((person, index) => {
+          return <Person
+          click= {() => deletePerson(index)}
+          name = {person.name} 
+          age={person.age}
+          key= {person.id}
+          changed= {(event) => nameChangeHandler(event, person.id)}
+          />
+        })}
+      </div>
+    );
+    style.backgroundColor = 'red';
+    style[':hover'] = {
+      backgroundColor: 'salmon',
+      color: 'black'
+    }
+  }
+
+
+  let classes = [];
+  if (personData.persons.length <= 2){
+    classes.push('red');
+  }
+  if (personData.persons.length <= 1){
+    classes.push('bold');
+  }
+
   return (
     <div className="App">
       <h1>react app</h1>
-      <button onClick={togglePersonHandler}>switch Name</button>
-      {personData.showPersons ?
-      <div>
-      <Person  
-          name= {personData.persons[0].name} 
-          age={personData.persons[0].age}
-          click = {switchHandler.bind(this, "Max_Using_bind")}/>
-        <Person 
-          name={personData.persons[1].name} 
-          age={personData.persons[1].age}
-          changed = {nameChangeHandler}>This is child</Person>
-        <Person 
-          name={personData.persons[2].name} 
-          age={personData.persons[2].age}/>
-      </div>
-      : null }
+      <p className={classes.join(' ')} > Delete Element Warnings </p>
+      <button
+        style = {style}
+        onClick={togglePersonHandler}>Toggle Persons</button> 
+      {persons}
     </div>
   );
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React  from 'react';
 import './Person.css';
+import Radium from 'radium'
 
 const Person = (props) => {
   return (
     <div className="Person">
       <p onClick={props.click}>I'm a person my name is {props.name} and age is {props.age}</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed}></input>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 }
